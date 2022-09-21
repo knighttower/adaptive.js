5a1184fc26709d12274997099c82fb18dae7d214
OrangePixel <maurix.suarez@outlook.com>
2022-09-20 22:05:13 -0400

    refactoring and adding methods --VERIFIED

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,14 @@
 const mix = require('laravel-mix');
 
-mix.js('example/app.js', 'test').vue();
+mix.js('example/app.js', 'example/app.min.js').vue();
 mix.js('src/Adaptive.js', 'dist');
-// mix.js('src/Adaptive.js', 'dist/Adaptive.min.js');
-mix.autoload({
-    jquery: ['$', 'window.jQuery', 'jQuery'],
-})
-    .webpackConfig({
-        externals: {
-            vue: 'Vue',
-        },
-        resolve: {
-            modules: ['node_modules'],
-        },
-        stats: 'errors-only',
-    })
-    .disableNotifications();
+mix.minify('src/Adaptive.js', 'dist/Adaptive.min.js');
+mix.webpackConfig({
+    externals: {
+        vue: 'Vue',
+    },
+    resolve: {
+        modules: ['node_modules'],
+    },
+    stats: 'errors-only',
+}).disableNotifications();
